bfe4280b9f7a032c118ae0ba3b40a9621a70a799
Shiva Dhakal <theskdhakal@gmail.com>
2023-10-11 11:02:18 +1100

    css updated on weather app

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+
+app.use(express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
 const api_key = "9eb1403073a634730b9ddd04a233631c";
 
